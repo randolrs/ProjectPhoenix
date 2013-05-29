a4e6f3d4ee94b1491b4e67aa459b7916284c1f18
Shane Randolph <r.shane.randolph@gmail.com>
2013-05-29 18:36:22 -0500

    Initial COmmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectPhoenix::Application.config.secret_token = 'a2864575a56ccf09eec54de16728061fb641c20a8bedcbfc6b2b87afbfbec29388cf5c10599f8b3122d7cec073eeb6fcfe796410245c1439f5e22bbd216231dd'
