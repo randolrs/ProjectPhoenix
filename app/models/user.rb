d7b686adc1f9ad5dfb62b2efbc208e69b7dec8ab
Shane Randolph <r.shane.randolph@gmail.com>
2013-07-19 22:39:44 -0500

    Fixed signup/login formats

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :music_type, :phone
   # attr_accessible :title, :body
 
-
   has_many :Gigs
 
 end
