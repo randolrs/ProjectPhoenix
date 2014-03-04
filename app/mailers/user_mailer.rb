3ea7256474bf893076586066ab63c83c8eb8ee1f
Shane Randolph <r.shane.randolph@gmail.com>
2014-03-03 20:50:47 -0600

    mailer issue

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,11 @@
 class UserMailer < ActionMailer::Base
-  default from: "Shane@GoSongBird.com"
 
-  def welcome_email(user)
-  	@user = user
-  	@url = 'http://gosongbird.com'
-  	mail(to:@user.email, subject 'Welcome to SongBird!')
-	end
+default from: "Shane@GoSongBird.com"
+
+ def welcome_email(user)
+	@user = user
+ 	@url = 'http://gosongbird.com'
+ 	mail(to:@user.email, subject:=> "Welcome to SongBird!"")
+end
 
 end
